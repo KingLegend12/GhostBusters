57d4c0f5956876e068d711a74d0b6a7d027b5788
DrissJD <85832488+KingLegend12@users.noreply.github.com>
2021-11-14 23:55:34 +0100

    In Progress

--- Assets/Script/Game.cs
+++ Assets/Script/Game.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
-    public GameObject[,] grid = new GameObject[6, 15];
+    public Tile[,] grid = new Tile[6, 15];
+    public GameObject[,] walls = new GameObject[7, 16];
     // Start is called before the first frame update
     void Start()
     {
         PlaceGhost();
-          
+       
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+    }
+    private void CheckInput() { 
+
+            if(Input.GetButtonDown("Fire1")) 
+            { 
+                    Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    int x = Mathf.RoundToInt(mousePosition.x);
+                    int y = Mathf.RoundToInt(mousePosition.y);
+                    Debug.Log(x+"  "+ y);
+                    Tile tile = grid[x, y];
+                    tile.SetIsCovered(false);
+            }
     }
 
     void PlaceGhost () 
@@ -26,11 +40,12 @@ public class Game : MonoBehaviour
 
             if( grid[x, y]==null)
              { 
-                GameObject ghostTile =  Instantiate(Resources.Load("Prefabs/ghostpic", typeof(GameObject)), new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+                Tile ghostTile =  Instantiate(Resources.Load("Prefabs/red", typeof(Tile)), new Vector3(x, y, 0), Quaternion.identity) as Tile;
                 grid[x, y]=ghostTile;
 
                 Debug.Log("("+x+", "+ y+ ")");
                 PlaceColor(x, y);
+                 
                 
             } else { 
                 PlaceGhost();
@@ -51,24 +66,23 @@ public class Game : MonoBehaviour
                     Distance=DistanceX+DistanceY;
 
                     if(Distance>=5) {
-                        GameObject color =  Instantiate(Resources.Load("Prefabs/green", typeof(GameObject)), new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+                        Tile color =  Instantiate(Resources.Load("Prefabs/green", typeof(Tile)), new Vector3(x, y, 0), Quaternion.identity) as Tile;
                         grid[x, y]=color;
                         }
                          else if (Distance==3 || Distance == 4) {
-                        GameObject color =  Instantiate(Resources.Load("Prefabs/yellow", typeof(GameObject)), new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+                        Tile color =  Instantiate(Resources.Load("Prefabs/yellow", typeof(Tile)), new Vector3(x, y, 0), Quaternion.identity) as Tile;
                         grid[x, y]=color;
                         }
                               else if (Distance==2 || Distance == 1) {
-                        GameObject color =  Instantiate(Resources.Load("Prefabs/orange", typeof(GameObject)), new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+                        Tile color =  Instantiate(Resources.Load("Prefabs/orange", typeof(Tile)), new Vector3(x, y, 0), Quaternion.identity) as Tile;
                         grid[x, y]=color;
                         }
-                                  else if (Distance==0) {
-                        GameObject color =  Instantiate(Resources.Load("Prefabs/red", typeof(GameObject)), new Vector3(x, y, 0), Quaternion.identity) as GameObject;
-                        grid[x, y]=color;
-                        }
+                                 
                                     
                 
             }
         }
     }
+
+   
 }
